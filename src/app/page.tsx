5490e8667f74977182c4f4e759fa8c13566e7cd0
Amine Djabri <djabri.aminee@gmail.com>
2026-02-19 15:17:10 +0100

    delete test buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,6 @@ export default function Home() {
   return (
     <div className="mx-auto">
 
-
-
-<Button variant="ghost" size="sm">
-  Contact
-</Button>
-<Button variant="primary" className="glow-box" size="lg">
-  Contact
-</Button>
-<Button variant="outline" size="lg">
-  Contact
-</Button>
-<Button variant="ghost" size="lg">
-  Contact
-</Button>
-
-
-<Button variant="secondary" className="">
-  Envoyer
-</Button>
     </div>
   );
 }
