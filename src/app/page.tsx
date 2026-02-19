fc212d94e6671ba5fddc3eba6b4fb9b95f3df38b
Amine Djabri <djabri.aminee@gmail.com>
2026-02-19 14:17:02 +0100

    add button ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import { Button } from "@/components/ui/Button";
 
 export default function Home() {
   return (
     <div className="pt-50">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minus quia eum beatae hic, nulla consequatur eveniet sapiente amet distinctio nam incidunt. Non nihil eius optio facilis! Saepe, esse ducimus!lore
-      
+    
+
+
+
+<Button variant="ghost" size="sm">
+  Contact
+</Button>
+<Button variant="primary" className="glow-box" size="lg">
+  Contact
+</Button>
+<Button variant="outline" size="lg">
+  Contact
+</Button>
+<Button variant="ghost" size="lg">
+  Contact
+</Button>
+
+
+<Button variant="secondary" className="">
+  Envoyer
+</Button>
     </div>
   );
 }
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { motion, HTMLMotionProps } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+type ButtonVariant = "primary" | "secondary" | "ghost" | "outline";
+type ButtonSize = "sm" | "md" | "lg";
+
+type ButtonProps = {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  className?: string;
+  children: React.ReactNode;
+} & HTMLMotionProps<"button">; 
+
+const variants = {
+  primary: "bg-primary text-primary-foreground hover:opacity-90",
+  secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+  ghost: "text-muted-foreground hover:text-foreground hover:bg-secondary/50",
+  outline: "border border-primary/50 bg-background text-foreground hover:bg-accent",
+};
+
+const sizes = {
+  sm: "px-3 py-1 text-sm",
+  md: "px-4 py-2 text-sm",
+  lg: "px-15 h-11 text-base",
+};
+
+export const Button = ({
+  variant = "primary",
+  size = "md",
+  className,
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <motion.button
+      whileHover={{ scale: 1.01 }}
+      whileTap={{ scale: 0.99 }}
+      className={cn(
+        "rounded-md font-medium transition-colors cursor-pointer",
+        variants[variant],
+        sizes[size],
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </motion.button>
+  );
+};
